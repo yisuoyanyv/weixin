2b833e6bb6ec1e1df5491f6b4f5f14af54a666d0
Nobody <nobody@yq150.onlinetools>
2017-10-07 22:09:56 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -22,7 +22,7 @@ class WeixinInterface:
         nonce=data.nonce
         echostr=data.echostr
         #自己的token
-        token="yangyanxing" #这里改写你在微信公众平台里输入的token
+        token="zhangjinglong" #这里改写你在微信公众平台里输入的token
         #字典序排序
         list=[token,timestamp,nonce]
         list.sort()
