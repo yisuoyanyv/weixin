c6b906949c8a3a2f3facb50331d7a1e2a3cebccb
Nobody <nobody@yq150.onlinetools>
2017-10-07 23:20:54 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -48,8 +48,8 @@ class WeixinInterface:
     	return self.render.reply_text(fromUser,toUser,int(time.time()),Nword)
     
     def youdao(q):
-        appKey = '68288d01f74b3f01'
-        secretKey = 'zRKls8HP3j3jeTZgFCYE2SzO9Xhp8jfi'
+        appKey = u'68288d01f74b3f01'
+        secretKey =u'zRKls8HP3j3jeTZgFCYE2SzO9Xhp8jfi'
 
 
         
