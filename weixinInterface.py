3b18965cb7e6553deb6e43b36a433ee6677b2e2b
Nobody <nobody@yq150.onlinetools>
2017-10-09 09:14:10 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -83,7 +83,7 @@ def youdao(q):
     m1.update(sign)
     sign = m1.hexdigest()
     
-    myurl = myurl+'?appKey='+appKey+'&q='+urllib2.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+    myurl = myurl+r'?appKey='+appKey+r'&q='+urllib2.quote(q)+r'&from='+fromLang+r'&to='+toLang+r'&salt='+str(salt)+r'&sign='+sign
 	return myurl
 
     #resp = urllib2.urlopen(myurl)
