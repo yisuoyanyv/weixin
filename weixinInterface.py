fa9577f7a64310930e0dce752f3111ad2b912c19
Nobody <nobody@yq150.onlinetools>
2017-10-09 10:22:45 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -88,13 +88,24 @@ def youdao(q):
 
     resp = urllib2.urlopen(myurl)
     fanyi = json.loads(resp.read())
-    ##根据json是否返回一个叫“basic”的key来判断是否翻译成功
-    if 'basic' in fanyi.keys():
-    ##下面是你自已来组织格式
-    	trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'],','.join(fanyi['translation']),','.join(fanyi['basic']['explains']),','.join(fanyi['web'][0]['value']))
-    	return trans
+    
+    if fanyi['errorCode'] == 0:    
+        ##根据json是否返回一个叫“basic”的key来判断是否翻译成功
+        if 'basic' in fanyi.keys():
+            trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'],''.join(fanyi['translation']),' '.join(fanyi['basic']['explains']),''.join(fanyi['web'][0]['value']))
+            return trans
+        else:
+            trans =u'%s:\n基本翻译:%s\n'%(fanyi['query'],''.join(fanyi['translation']))        
+            return trans
+    elif fanyi['errorCode'] == 20:
+        return u'对不起，要翻译的文本过长'
+    elif fanyi['errorCode'] == 30:
+        return u'对不起，无法进行有效的翻译'
+    elif fanyi['errorCode'] == 40:
+        return u'对不起，不支持的语言类型'
     else:
-    	return u'对不起，您输入的单词%s无法翻译，请检查拼写'% q
+        return u'对不起，您输入的单词%s无法翻译,请检查拼写'% q
+    
     
     
     
